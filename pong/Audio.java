b762b0ffd736afc41a041e20816e422717ce7f28
macarlino <carlino.matthew@knights.ucf.edu>
2017-04-08 21:35:24 -0400

    Create Audio.java

---
+++ pong/Audio.java
@@ -0,0 +1,67 @@
+package pong;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.FloatControl;
+
+public class Audio {
+
+    private ArrayList<String> musicFiles;
+    private int currentSongIndex;
+
+    public Audio(String... files){
+        musicFiles = new ArrayList<String>();
+
+        //Specify proper file path and have .wav audio file
+        for(String file : files)
+            musicFiles.add("./pong/" + file + ".wav");
+    }
+
+    private void playBGM(String fileName){
+
+        try{
+            File soundFile = new File(fileName);
+            AudioInputStream ais = AudioSystem.getAudioInputStream(soundFile);
+            AudioFormat format = ais.getFormat();
+            DataLine.Info info = new DataLine.Info(Clip.class, format);
+            Clip clip = (Clip)AudioSystem.getLine(info);
+            clip.open(ais);
+
+            FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            gainControl.setValue(-20);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+            Thread.sleep(1000);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void playSound(String fileName) {
+        try{
+            File soundFile = new File(fileName);
+            AudioInputStream ais = AudioSystem.getAudioInputStream(soundFile);
+            AudioFormat format = ais.getFormat();
+            DataLine.Info info = new DataLine.Info(Clip.class, format);
+            Clip clip = (Clip)AudioSystem.getLine(info);
+            clip.open(ais);
+
+            FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            gainControl.setValue(-20);
+            clip.start();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void runSound() { playSound(musicFiles.get(currentSongIndex));}
+
+    public void runBGM() {
+        playBGM(musicFiles.get(currentSongIndex));
+    }
+}
