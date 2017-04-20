5532c77f483197f87e1f369f526a5a11a0a28ec9
tfreeman91 <tfreeman91@users.noreply.github.com>
2017-04-20 16:13:01 -0400

    Update MainMenu.java

--- pong/MainMenu.java
+++ pong/MainMenu.java
@@ -16,7 +16,7 @@ public class MainMenu {
 	private JFrame levelMenu;
 	private JFrame options;
 	private JFrame help;
-	
+	public JFrame jfrm;
 	
 	// buttons for main menu
 	private JButton p1;
@@ -180,7 +180,7 @@ public class MainMenu {
 		title.setFont(new Font("Verdana",1, 20));
 		top.add(title);
 		top.setBorder(new LineBorder(Color.BLACK));
-		 menu.add(top);
+		menu.add(top);
 		top.setVisible(true);
 		//menu.add(top);
 		
@@ -233,6 +233,8 @@ public class MainMenu {
 			{
 				menu.setVisible(false);
 				levelMenu.setVisible(true);
+				//Pong pong = new Pong();
+				
 			}
 		});
 		
@@ -296,6 +298,7 @@ public class MainMenu {
 				levelMenu.setVisible(false);
 				// open level 1
 				Pong p = new Pong();
+				p.changeDifficulty(1);
 				
 			}
 		});
@@ -306,7 +309,8 @@ public class MainMenu {
 				if(getLevelProgress() > 1)
 				{
 					levelMenu.setVisible(false);
-					// open level 2
+					Pong p = new Pong();
+					p.changeDifficulty(2);
 				}
 			}
 		});
@@ -317,7 +321,8 @@ public class MainMenu {
 				if(getLevelProgress() > 2)
 				{
 					levelMenu.setVisible(false);
-					// open level 3
+					Pong p = new Pong();
+					p.changeDifficulty(3);
 			
 				}
 			}
@@ -329,6 +334,9 @@ public class MainMenu {
 				if(getLevelProgress() > 3)
 				{
 					levelMenu.setVisible(false);
+					Pong p = new Pong();
+					p.changeDifficulty(4);
+					
 					// open level 3
 				}
 			}
@@ -340,7 +348,8 @@ public class MainMenu {
 				if(getLevelProgress() > 4)
 				{	
 					levelMenu.setVisible(false);
-					// open level 3
+					Pong p = new Pong();
+					p.changeDifficulty(5);
 				}
 			}
 		});
@@ -351,7 +360,8 @@ public class MainMenu {
 				if(getLevelProgress() > 5)
 				{
 					levelMenu.setVisible(false);
-					// open level 3
+					Pong p = new Pong();
+					p.changeDifficulty(6);
 				}
 			}
 		});
@@ -383,40 +393,74 @@ public class MainMenu {
 	public void showHelpMenu()
 	{
 		
-		help.setVisible(true);
-
-		JPanel helpPanel = new JPanel(new BorderLayout());
-		JPanel helpGrid = new JPanel(new GridLayout(2, 4, 10, 10));
+		//JFrame helpScreen = new JFrame("HELP");
 		
+		
+		// setting up the main menu
 		JPanel helpTop = new JPanel();
-		JButton helpTitle = new JButton("HELP");
-		helpTitle.setFont(new Font("Verdana", 1, 20));
-		helpTop.add(helpTitle);
-		helpTop.setBorder(new LineBorder(Color.BLACK));
-		help.add(helpTop);
-		helpTop.setVisible(true);
+		JLabel helpTitle = new JLabel("HELP");
+		//helpTitle.setFont(new Font("Verdana",1, 20));
+		//helpTop.add(helpTitle);
+		//helpTop.setBorder(new LineBorder(Color.BLACK));
+		//help.add(helpTop);
+		//helpTop.setVisible(true);
+		//helpTop.setSize(50, 50);
+		//help.add(helpTop);
+		//help.setVisible(true);
 		
-		JButton helpTitleText = new JButton("HELP SCREEN");
-		helpTitleText.setBorder(new LineBorder(Color.black));
-		helpTitleText.setOpaque(false);
-		helpTitleText.setContentAreaFilled(false);
-		helpTitleText.setBorderPainted(false);
-		helpTitleText.setFont(new Font("Verdana", Font.PLAIN, 25));
 		
+        //helpTop.setSize(300,400);
+        help.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel = new JPanel(new GridLayout(6, 2));
+        help.add(panel, BorderLayout.CENTER);
+
+        JLabel txt = new JLabel("HELP", JLabel.CENTER);
+//        txt.setLayout(new GridLayout(1, 1));
+        txt.setHorizontalTextPosition(JLabel.CENTER);
+        txt.setFont(new Font("Serif", Font.PLAIN, 60));
+//        window.add(txt);
+        help.add(txt, BorderLayout.NORTH);
+        help.setVisible(true);
+
 		JButton item1 = new JButton("item1.png");
-		JButton item1Desc = new JButton("<html>Item 1 Description is that<br> is does things");
+		JButton item1Desc = new JButton("<html>Increases your paddle size");
 		JButton item2 = new JButton("item2.png");
-		JButton item2Desc = new JButton("<html>Item 2 Description is that<br> is does things");
+		JButton item2Desc = new JButton("<html>Increases your score by one point");
 		JButton item3 = new JButton("item3.png");
-		JButton item3Desc = new JButton("<html>Item 3 Description is that<br> is does things");
+		JButton item3Desc = new JButton("<html>Damages your paddle");
 		JButton item4 = new JButton("item4.png");
-		JButton item4Desc = new JButton("<html>Item 4 Description is that<br> is does things");
-		JButton item5 = new JButton("item5.png");
-		JButton item5Desc = new JButton("<html>Item 5 Description is that<br> is does things");
-		JButton item6 = new JButton("item6.png");
-		JButton item6Desc = new JButton("<html>Item 6 Description is that<br> is does things");
+		JButton item4Desc = new JButton("<html>Replenishes your health");
 		
-		JPanel helpInner = new JPanel(new GridLayout(6, 2, 0, 20));
+		panel.add(item1).setLocation(0, 0);
+		panel.add(item1Desc).setLocation(0, 1);
+		item1Desc.setOpaque(false);
+		item1Desc.setContentAreaFilled(false);
+		item1Desc.setBorderPainted(false);
+		panel.add(item2).setLocation(1, 0);
+		panel.add(item2Desc).setLocation(1, 1);
+		item2Desc.setOpaque(false);
+		item2Desc.setContentAreaFilled(false);
+		item2Desc.setBorderPainted(false);
+		panel.add(item3).setLocation(2, 0);
+		panel.add(item3Desc).setLocation(2, 1);
+		item3Desc.setOpaque(false);
+		item3Desc.setContentAreaFilled(false);
+		item3Desc.setBorderPainted(false);
+		panel.add(item4).setLocation(3, 0);
+		panel.add(item4Desc).setLocation(3, 1);
+		item4Desc.setOpaque(false);
+		item4Desc.setContentAreaFilled(false);
+		item4Desc.setBorderPainted(false);
+		
+		JButton helpBack = new JButton("BACK");
+		panel.add(helpBack).setLocation(4, 0);
+		JButton helpQuit = new JButton("QUIT");
+		panel.add(helpQuit).setLocation(4, 1);
+		
+		// title of game
+	
+		// set items in grid
+/*
 		helpInner.add(item1).setLocation(0, 0);
 		helpInner.add(item1Desc).setLocation(0, 1);
 		item1Desc.setOpaque(false);
@@ -424,141 +468,29 @@ public class MainMenu {
 		item1Desc.setBorderPainted(false);
 		helpInner.add(item2).setLocation(1, 0);
 		helpInner.add(item2Desc).setLocation(1, 1);
+		item2Desc.setOpaque(false);
+		item2Desc.setContentAreaFilled(false);
+		item2Desc.setBorderPainted(false);
+		
 		helpInner.add(item3).setLocation(2, 0);
 		helpInner.add(item3Desc).setLocation(2, 1);
+		item3Desc.setOpaque(false);
+		item3Desc.setContentAreaFilled(false);
+		item3Desc.setBorderPainted(false);
+		
 		helpInner.add(item4).setLocation(3, 0);
 		helpInner.add(item4Desc).setLocation(3, 1);
-		helpInner.add(item5).setLocation(4, 0);
-		helpInner.add(item5Desc).setLocation(4, 1);
-		helpInner.add(item6).setLocation(5, 0);
-		helpInner.add(item6Desc).setLocation(5, 1);
-		helpInner.setBorder(BorderFactory.createEmptyBorder(100, 50, 100, 50));
-		
-		/*
-		helpInner.add(Box.createRigidArea(new Dimension(5, 1)));
-		helpInner.add(item1).setLocation(0, 0);
-		helpInner.add(item1Desc).setLocation(0, 1);
-		helpInner.add(item2).setLocation(1, 0);
-		helpInner.add(item2Desc).setLocation(1, 1);
-		helpInner.add(item3).setLocation(2, 0);
-		helpInner.add(item3Desc).setLocation(2, 1);
-		helpInner.add(item4).setLocation(3, 0);
-		helpInner.add(item4Desc).setLocation(3, 1);
-		helpInner.add(item5).setLocation(4, 0);
-		helpInner.add(item5Desc).setLocation(4, 1);
-		helpInner.add(item6).setLocation(5, 0);
-		helpInner.add(item6Desc).setLocation(5, 1);
-		helpInner.setBorder(BorderFactory.createEmptyBorder(200, 400, 200, 400));
-		*/
-		help.add(helpInner);
-		
-		JLabel instLabel = new JLabel("<html>"
-				+ "HOW TO PLAY<br>"
-				+ "The goal of the game is to out-survive, and "
-				+ "out-score your opponent.<br>"
-				+ "To do this, control your paddle to deflect the ball away from your base. <br>"
-				+ "Watch out for items though! While some can help you, others will do damage, ensuring "
-				+ "a quick victory for your opponent.<br> "
-				+ ""
-				+ "</html>");
-		
-		//helpPanel.add(instLabel);
-		
-	
-		/*
-		// setting up the main menu
-		JPanel top = new JPanel();
-		JLabel title = new JLabel("Breakout Ball");
-		title.setFont(new Font("Verdana",1, 20));
-		top.add(title);
-		top.setBorder(new LineBorder(Color.BLACK));
-		menu.add(top);
-		top.setVisible(true);
-		//menu.add(top);
-		
-		// title of game
-		titleText = new JButton("Breakout Ball");
-		titleText.setBorder(new LineBorder(Color.black));
-		titleText.setOpaque(false);
-		titleText.setContentAreaFilled(false);
-		titleText.setBorderPainted(false);
-		titleText.setFont(new Font("Verdana", Font.PLAIN, 25));
-		
-		// buttons for menu navigation
-		p1 = new JButton("1 Player");
-		p1.setBorder(new LineBorder(Color.BLACK));
-		p1.setBackground(Color.WHITE);
-		p1.setOpaque(true);
-		p2 = new JButton("2 Player");
-		p2.setBorder(new LineBorder(Color.BLACK));
-		p2.setBackground(Color.WHITE);
-		p2.setOpaque(true);
-		op = new JButton("Options");
-		op.setBorder(new LineBorder(Color.BLACK));
-		op.setBackground(Color.WHITE);
-		op.setOpaque(true);
-		hp = new JButton("Help");
-		hp.setBorder(new LineBorder(Color.BLACK));
-		hp.setBackground(Color.WHITE);
-		hp.setOpaque(true);
-		qt = new JButton("Quit");
-		qt.setBorder(new LineBorder(Color.BLACK));
-		qt.setBackground(Color.WHITE);
-		qt.setOpaque(true);	
-		// adding proper bits into main panel
-		JPanel innerPanel = new JPanel(new GridLayout(0, 1, 50, 0));
-		innerPanel.add(titleText);
-		innerPanel.add(Box.createRigidArea(new Dimension(5, 10)));
-		innerPanel.add(p1);
-		innerPanel.add(p2);
-		innerPanel.add(op);
-		innerPanel.add(hp);
-		innerPanel.add(qt);
-		innerPanel.setOpaque(false);
-		innerPanel.setBorder(BorderFactory.createEmptyBorder(200, 100, 200, 100));
-		
-		menu.add(innerPanel);
-		 */
-		
-		
-		
-		/*
-		g.setColor(Color.WHITE);
-        g.setFont(new Font("Arial", 1, 50));
-        g.drawString("HELP MENU", width / 2 - 180, height / 2 - 250);
-        g.setFont(new Font("Arial", 1, 20));
-        String nav = "Navigation";
-        String paddle = "Paddle";
-        String close = "Close Help";
-        String navSpace = "space - multiplayer";
-        String navShift = "shift - singleplayer";
-        String navChoose = "a/d or left/right arrows to choose settings";
-        String player1String = "Player 1 - w (up), s (down)";
-        String player2String = "Player 2 - up arrow (up), down arrow (down)";
-        g.drawString(nav, 100, height / 2 - 180);
-        g.drawString(navSpace, 150, height / 2 - 140);
-        g.drawString(navShift, 150, height / 2 - 100);
-        g.drawString(navChoose, 150, height / 2 - 60);
-        g.drawString(paddle, 100, height / 2 - 20);
-        g.drawString(player1String, 150, height / 2 + 20);
-        g.drawString(player2String, 150, height / 2 + 60);
-        g.drawString(close, 100, height / 2 + 100);
-        g.drawString("h", 150, height / 2 + 140);
-        
-        /*
-         * 				HELP MENU
-         * Navigation
-         * 		space - multiplayer
-         * 		shift - single player
-         * 		a/d or left/right arrows to choose settings
-         * 
-         * Paddle
-         * 		Player 1 - W (up), S (down)
-         * 		Player 2 - Up Arrow (up), Down Arrow (down)
-         * Close Help
-         * 		h
-         * 
-         */
+		item4Desc.setOpaque(false);
+		item4Desc.setContentAreaFilled(false);
+		item4Desc.setBorderPainted(false);
+		helpInner.setOpaque(false);
+		JButton helpBack = new JButton("BACK");
+		helpInner.add(helpBack).setLocation(4, 0);
+		JButton helpQuit = new JButton("QUIT");
+		helpInner.add(helpQuit).setLocation(4, 1);
+*/
+		 
+
 	}
 	
 	public static void setSound(int level)
@@ -568,8 +500,13 @@ public class MainMenu {
 			return;
 		if(currentLevel == 2 && level == 1)
 		{
-			
+			decreaseSound(currentLevel, level);
 		}
+	}
+	
+	public static void decreaseSound(int currentLevel, int leveL)
+	{
+		
 	}
 	
 	// creating new image
